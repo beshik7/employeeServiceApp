209a0439d5351d5108019707ee06ddb124db9570
beshik7 <tommypdx7@gmail.com>
2023-06-11 22:57:01 -0700

    Libraries homeWork is done

--- src/main/java/com/sky/pro/employeeservice/EmployeeService.java
+++ src/main/java/com/sky/pro/employeeservice/EmployeeService.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 public interface EmployeeService {
     void addEmployee(Employee employee);
     void removeEmployee(String firstName, String lastName);
-    void findEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
 
 
     Collection<Employee> getAllEmployees();
--- src/main/java/com/sky/pro/employeeservice/EmployeeServiceImpl.java
+++ src/main/java/com/sky/pro/employeeservice/EmployeeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.pro.employeeservice;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -10,32 +11,49 @@ public class EmployeeServiceImpl implements EmployeeService{
     private Map<String, Employee> storage = new HashMap<>();
     @Override
     public void addEmployee(Employee employee) {
-        String key = employee.getFirstName() + employee.getLastName();
+        String firstName = StringUtils.capitalize(employee.getFirstName());
+        String lastName = StringUtils.capitalize(employee.getLastName());
+        String key = firstName + lastName;
         if (storage.containsKey(key)) {
             throw new EmployeeAlreadyAddedException(" Сотрудник уже добавлен ");
+        }
+        if (!StringUtils.isAlpha(employee.getFirstName()) || !StringUtils.isAlpha(employee.getLastName())) {
+            throw new UnexpectedCharacterException("Names should contain only alphabetic characters");
         }
         storage.put(key, employee);
     }
 
     @Override
     public void removeEmployee(String firstName, String lastName) {
-        if (storage.containsKey(firstName + lastName)) {
+        String key = firstName + lastName;
+
+        if (!storage.containsKey(key)) {
             throw new EmployeeNotFoundException(" Сотрудник не найден ");
         }
         storage.remove(firstName + lastName);
     }
 
     @Override
-    public void findEmployee(String firstName, String lastName) {
-        if (storage.containsKey(firstName + lastName)) {
+    public Employee findEmployee(String firstName, String lastName) {
+        String key = firstName +lastName;
+
+        if (!storage.containsKey(key)) {
             throw new EmployeeNotFoundException(" Сотрудник не найден ");
         }
-        storage.get(firstName + lastName);
+        return storage.get(firstName + lastName);
 
     }
 
     @Override
     public Collection<Employee> getAllEmployees() {
         return storage.values();
+    }
+
+    private void validate(String... values) {
+        for (String value : values) {
+            if (!StringUtils.isAlpha(value)) {
+                throw new UnexpectedCharacterException("Bad request in" + value);
+            }
+        }
     }
 }
